0f79c8a1f825212b5dafe47ecfff842ec860f5db
MJaved9 <101566706+MJaved9@users.noreply.github.com>
2022-05-19 19:55:09 +0530

    Add files via upload

---
+++ day-1-A1-1/main/src/App.js
@@ -0,0 +1,30 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="nav">
+        logobackary
+        <a hreh="#">Service</a>
+        <a hreh="#">Project</a>
+        <a hreh="#">About us</a>
+        <button>Contact</button>
+      </div>
+     <nav>
+       <h1>Mobile Operating System</h1>
+         <li>Android</li>
+         <li>Blackbery</li>
+         <li>iPhone</li>
+         <li>Windows Phone</li>
+         <h1>Mobile Manufacturers</h1>
+         <li>Micromax</li>
+         <li>Micromax</li>
+         <li>Samsung</li>
+         <li>HTC</li>
+     </nav>
+    </div>
+  );
+}
+
+export default App;
